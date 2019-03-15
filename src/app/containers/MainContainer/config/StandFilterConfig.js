1b8efc23736a2d2f6b6c24ba9d6b09462733d95b
CORP\pankajg <pankaj.goyal@optym.com>
2019-03-15 16:39:05 +0530

    Some config changes

--- src/app/containers/MainContainer/config/StandFilterConfig.js
+++ src/app/containers/MainContainer/config/StandFilterConfig.js
@@ -1,7 +1,9 @@
 import Fields from '../../../constants/Fields';
+import FilterType from '../../../constants/FilterType';
 
 export default {
   title: 'Stands',
+  defaultFilterType: FilterType.ABSOLUTE_FILTER,
   items: [],
   filterLogic: (r, selectedValues) => {
     if (selectedValues && selectedValues.length > 0) {
@@ -9,4 +11,5 @@ export default {
     }
     return true;
   },
+  headerIconClass:'span-header-image-stand'
 };
